4d9fb5c452023777d7ae58834ea4eaad6074306b
hwangd-cohere <dean.c.hwang@gmail.com>
2023-04-24 16:31:53 -0400

    bootstrap using standalone APIs

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,20 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AuthConfigModule } from './app/authconfig/auth.config.module';
+import { AstroComponentsModule } from '@astrouxds/angular';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app/app-routing.module';
+import { CommonModule } from '@angular/common';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, CommonModule, AppRoutingModule, AstroComponentsModule, AuthConfigModule),
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
   .catch(err => console.error(err));
